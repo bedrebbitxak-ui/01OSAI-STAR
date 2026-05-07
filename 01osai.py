685c3fb722beeebef210c137522b58ad7c700be0
bedrebbitxak-ui <bedrebbit.xak@gmail.com>
2026-05-07 06:33:14 +0500

    Update 01osai.py

--- 01osai.py
+++ 01osai.py
@@ -1,29 +1,31 @@
-import time
+from core.utils import log
 from memory_01 import Memory01
 from safety import Safety
 from shell_01 import Shell01
-from module_loader import ModuleLoader
+from core.module_loader import ModuleLoader
 
-def log(msg):
-    print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {msg}")
 
 def main():
     log("01OSAI: initializing system")
 
+    # память и безопасность
     memory = Memory01()
     safety = Safety()
+    log("MEMORY: loaded")
+    log("SAFETY: initialized")
 
+    # загрузка модулей
     loader = ModuleLoader()
-
     log("01OSAI: loading modules")
     modules = loader.load_modules()
 
+    # запуск shell
     shell = Shell01(memory, modules)
-
     log("01OSAI: system ready")
     log("01OSAI: interactive shell started")
 
     shell.run()
+
 
 if __name__ == "__main__":
     main()
